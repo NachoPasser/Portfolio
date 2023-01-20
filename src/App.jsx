3813b23ccdb55f7a9adf356baeaa43e855a80b31
NachoPasser <nachopasser@gmail.com>
2023-01-20 17:24:53 -0300

    refact(App): /home por /Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import {Grid} from '@chakra-ui/react'
-import Home from './components/Home/home'
+import Home from './components/Home/Home'
 import NavBar from './components/NavBar/NavBar'
 import './App.css'
 
