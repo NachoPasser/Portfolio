9e2bf99a8b881c1785e6ebeb0e8c01e9937b544e
NachoPasser <nachopasser@gmail.com>
2023-01-27 17:54:19 -0300

    fix(CustomSlider): arreglo de iteracion de imagenes

--- src/components/Proyects/Proyects/CustomSlider/CustomSlider.jsx
+++ src/components/Proyects/Proyects/CustomSlider/CustomSlider.jsx
@@ -1,51 +1,41 @@
 import React, { useState, useEffect } from 'react'
-import { Button, Flex, Image, keyframes } from '@chakra-ui/react';
+import { Button, Flex, Image} from '@chakra-ui/react';
 import { MdOutlineKeyboardArrowRight, MdOutlineKeyboardArrowLeft } from "react-icons/md";
 
 let count = 0
 function CustomSlider({sliderData, flexSize, isOpen}) {
     const [index, setIndex] = useState(0)
-    const [animating, setAnimating] = useState(false)
+    const [transitioning, setTransitioning] = useState(false)
     const [mounted, setMount] = useState(false)
-    const [images, setImages] = useState(null)
-
-
-    const fadeIn = keyframes`
-      from {
-        opacity: 0;
-      }
-      to {
-      opacity: 1;
-    }
-    `
 
     const next = () => {
-      if(!animating){
+      if(!transitioning){
+        console.log(count)
         count = (count + 1) % sliderData.length
         setIndex(count)
-        setAnimating(true)
+        setTransitioning(true)
       }
       if(!mounted) setMount(true)
     }
     
     const prev = () => {
-      if(!animating){
+      if(!transitioning){
         count = (index + sliderData.length - 1) % sliderData.length
         setIndex(count)
-        setAnimating(true)
+        setTransitioning(true)
       }
       if(!mounted) setMount(true)
     }
     
-    const onAnimationEnd = () => {
-      setAnimating(false)
+    const onTransitionEnd = () => {
+      setTransitioning(false)
     }
 
     const changeSlide = (id) => {
-      if(!animating){
+      if(!transitioning){
         count = id
         setIndex(id)
-        setAnimating(true)
+        setTransitioning(true)
       }
       if(!mounted) setMount(true)
     }
@@ -64,29 +54,29 @@ function CustomSlider({sliderData, flexSize, isOpen}) {
     }, [isOpen])
 
     useEffect(() => {
-      sliderData.forEach((image) => {
-        const newImage = document.createElement('img')
-        newImage.src = image;
-        window[image] = newImage;
-    });
+      return () => {
+        count = 0
+      }
     }, [])
+
+
 
   return (
     <Flex align={'center'} justifyContent='center' w='100%'position={'relative'} userSelect='none'>
       <Flex position={'relative'} justify={'center'} align='center'>
-        <Flex justify={'center'} align='center' position={'absolute'} left='10px' background={'white'} borderRadius={'50px'} border='1px solid #00fff7' onClick={prev} zIndex='100' _hover={{cursor: animating ? 'default' : 'pointer'}}>
+        <Flex justify={'center'} align='center' position={'absolute'} left='10px' background={'white'} borderRadius={'50px'} border='1px solid #00fff7' onClick={prev} zIndex='100' _hover={{cursor: transitioning ? 'default' : 'pointer'}}>
           <MdOutlineKeyboardArrowLeft stroke='#FFFFFF' fill='#000000' size={'30px'}/>
         </Flex>
         <Flex flex={flexSize[index]} >
           {sliderData?.map((img, i) => 
-          <Image key={i} src={img} w={'100%'} verticalAlign='middle' position={i !== index ? 'absolute' : 'relative'} opacity={i !== index ? 0 : 1} transition='all 1s' onTransitionEnd={onAnimationEnd}  minW={'300px'} minH={'150px'} />)}
+          <Image key={i} src={img} w={'100%'} verticalAlign='middle' position={i !== index ? 'absolute' : 'relative'} opacity={i !== index ? 0 : 1} transition='all 1s' onTransitionEnd={onTransitionEnd}  minW={'300px'} minH={'150px'} />)}
         </Flex>
-        <Flex justify={'center'} align='center' position={'absolute'}  right='10px' background={'white'} borderRadius={'50px'} border='1px solid #00fff7' onClick={next} zIndex='100' _hover={{cursor: animating ? 'default' : 'pointer'}}>
+        <Flex justify={'center'} align='center' position={'absolute'}  right='10px' background={'white'} borderRadius={'50px'} border='1px solid #00fff7' onClick={next} zIndex='100' _hover={{cursor: transitioning ? 'default' : 'pointer'}}>
           <MdOutlineKeyboardArrowRight stroke='#FFFFFF' fill='#000000' size={'30px'}/>
         </Flex>
         <Flex position={'absolute'} bottom={3} gap={2}>
           {sliderData.map((_, id) => 
-          <Button key={id} border={index === id ? '1px solid #00fff7' : '1px solid black'} w='10px' h='8px' background={index === id ? '#FCFBFE' : 'lightgrey'} _hover={{background: 'white', cursor: animating ? 'default' : 'pointer'}} onClick={() => changeSlide(id)}/>)}
+          <Button key={id} border={index === id ? '1px solid #00fff7' : '1px solid black'} w='10px' h='8px' background={index === id ? '#FCFBFE' : 'lightgrey'} _hover={{background: 'white', cursor: transitioning ? 'default' : 'pointer'}} onClick={() => changeSlide(id)}/>)}
         </Flex>
       </Flex>
     </Flex>
